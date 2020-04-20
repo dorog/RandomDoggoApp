72af2f49b236c6e4d986dfa0fd25a8ba180185a2
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2020-04-20 09:37:00 +0200

    Add Entity

--- app/src/main/java/bme/hu/randomdoggo/model/RandomDoggo.kt
+++ app/src/main/java/bme/hu/randomdoggo/model/RandomDoggo.kt
@@ -1,18 +1,26 @@
 package bme.hu.randomdoggo.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class RandomDoggo(
 
+    @PrimaryKey(autoGenerate = true)
     @SerializedName("id")
-    var id: Int,
+    var id: Int? = null,
 
+    @ColumnInfo()
     @SerializedName("url")
     var url: String,
 
+    @ColumnInfo()
     @SerializedName("fileSizeBytes")
     var byte: Int,
 
+    @ColumnInfo()
     @SerializedName("type")
     var type: String
 )

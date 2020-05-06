2f3f67ebc97a55e46209b331235cfba1bdf0e9a7
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2020-05-06 10:01:16 +0200

    DetailsFragmentDialog Refactor

--- app/src/main/java/bme/hu/randomdoggo/ui/details/DetailsFragmentDialog.kt
+++ app/src/main/java/bme/hu/randomdoggo/ui/details/DetailsFragmentDialog.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import bme.hu.randomdoggo.R
 import bme.hu.randomdoggo.model.RandomDoggo
@@ -13,10 +12,10 @@ import kotlinx.android.synthetic.main.fragment_details.view.*
 
 class DetailsDialogFragment : DialogFragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_details, container)
+    private var byteFormat: String = "%02d byte"
 
-        return view
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_details, container)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -26,14 +25,9 @@ class DetailsDialogFragment : DialogFragment() {
         val byte = arguments!!.getInt(byte, -1)
         val type = arguments!!.getString(type, "Missing Type")
 
-        val urlText = view.findViewById<View>(R.id.url) as TextView
-        urlText.text = url
-
-        val byteText = view.findViewById<View>(R.id.fileSizeBytes) as TextView
-        byteText.text = "$byte byte"
-
-        val typeText = view.findViewById<View>(R.id.type) as TextView
-        typeText.text = type
+        view.url.text = url
+        view.fileSizeBytes.text = String.format(byteFormat, byte)
+        view.type.text = type
 
         Glide.with(view.details_imageView)
                 .load(url)
@@ -52,11 +46,13 @@ class DetailsDialogFragment : DialogFragment() {
 
         fun newInstance(randomDoggo: RandomDoggo): DetailsDialogFragment {
             val frag = DetailsDialogFragment()
+
             val args = Bundle()
             args.putString(url, randomDoggo.url)
             args.putInt(byte, randomDoggo.fileSizeBytes)
             args.putString(type, randomDoggo.type)
             frag.arguments = args
+
             return frag
         }
     }

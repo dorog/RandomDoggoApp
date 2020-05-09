af69b03f3a78a1a3eadd3e076635c41c4ab0a112
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2020-05-09 09:15:30 +0200

    Delete commented codes

--- app/src/main/java/bme/hu/randomdoggo/ui/favourites/FavouritesPresenter.kt
+++ app/src/main/java/bme/hu/randomdoggo/ui/favourites/FavouritesPresenter.kt
@@ -6,16 +6,10 @@ import bme.hu.randomdoggo.model.RandomDoggo
 import bme.hu.randomdoggo.ui.Presenter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.coroutines.CoroutineContext
 
 class FavouritesPresenter @Inject constructor(private var randomDoggoRepository: RandomDoggoRepository, private var scope: CoroutineScope): Presenter<FavouritesScreen>() {
-
-    /*private var parentJob = Job()
-    private val coroutineContext: CoroutineContext get() = parentJob + Dispatchers.Main
-    private val scope = CoroutineScope(coroutineContext)*/
 
     fun removeRandomDoggoFromDatabase(randomDoggo: RandomDoggo) = scope.launch(Dispatchers.IO){
         randomDoggoRepository.removeRandomDoggo(randomDoggo)
--- app/src/main/java/bme/hu/randomdoggo/ui/search/SearchPresenter.kt
+++ app/src/main/java/bme/hu/randomdoggo/ui/search/SearchPresenter.kt
@@ -7,23 +7,17 @@ import bme.hu.randomdoggo.model.RandomDoggo
 import bme.hu.randomdoggo.ui.Presenter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import java.util.concurrent.Executor
 import javax.inject.Inject
-import kotlin.coroutines.CoroutineContext
 
 class SearchPresenter  @Inject constructor(private val executor: Executor,
                                            private val randomDoggoInteractor: RandomDoggoInteractor,
                                            private var randomDoggoRepository: RandomDoggoRepository,
                                            private var scope: CoroutineScope): Presenter<SearchScreen>() {
-
-    /*private var parentJob = Job()
-    private val coroutineContext: CoroutineContext get() = parentJob + Dispatchers.Main
-    private val scope = CoroutineScope(coroutineContext)*/
 
     override fun attachScreen(screen: SearchScreen) {
         super.attachScreen(screen)
